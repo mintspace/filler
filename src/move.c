ee1e6977a6d62c4f6fe17d90b68ee84c84454108
Dmitry Bubnov <dmitrybubnov.ca@gmail.com>
2019-08-13 02:18:57 -0700

    figure place found

--- src/move.c
+++ src/move.c
@@ -6,7 +6,7 @@
 /*   By: metalium <metalium@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/12 17:13:17 by dbubnov           #+#    #+#             */
-/*   Updated: 2019/08/13 00:54:55 by metalium         ###   ########.fr       */
+/*   Updated: 2019/08/13 02:18:10 by metalium         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,33 @@ void	move(t_map *map, int start_x, int start_y)
 {
 	int	i;
 	int j;
+	int x;
+	int y;
 
 	i = 0;
-	while (start_y < (map->plateau_y - map->piece_y) && i < map->piece_y)
+	x = start_x;
+	y = start_y;
+	while (y < ((map->plateau_y + 1) - map->piece_y) && i < map->piece_y)
 	{
 		j = 0;
-		while (start_x < (map->plateau_x - map->piece_x) && j < map->piece_x)
+		x = start_x;
+		while (x < ((map->plateau_y + 1)- map->piece_x) && j < map->piece_x)
 		{
-			if (map->figure_map[i][j] == '*' && map->plate_int[start_y][start_x] == 0)
+			// if (map->figure_map[i][j] == '*' && map->plate_int[start_y][start_x] == 0)
+			// {
+			// 	ft_printf("enemy: y = %d, x = %d\n", start_y, start_x);
+			// 	break ;
+			// }
+			if (map->figure_map[i][j] == '*' && map->plate_int[y][x] == -4)
 			{
-				ft_printf("enemy: y = %d, x = %d\n", start_y, start_x);
+				ft_printf("player: y = %d, x = %d\n", start_y, start_x);
 				break ;
 			}
 			j++;
-			start_x++;
+			x++;
 		}
 		i++;
-		start_y++;
+		y++;
 	}
 }
 void	check_figure(t_map *map)
@@ -41,10 +51,10 @@ void	check_figure(t_map *map)
 	int start_y;
 
 	start_y = 0;
-	while (start_y < (map->plateau_x - map->piece_x))
+	while (start_y < ((map->plateau_x + 1) - map->piece_x))
 	{
 		start_x = 0;
-		while (start_x < (map->plateau_y - map->piece_y))
+		while (start_x < ((map->plateau_y + 1) - map->piece_y))
 		{
 			move(map, start_x, start_y);
 			start_x++;
@@ -52,6 +62,3 @@ void	check_figure(t_map *map)
 		start_y++;
 	}
 }
-
-
-
